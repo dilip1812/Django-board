7a2be249a6c7f114df7f26ab00d6c507b7d859fb
dilip1812 <dcluck1812@gmail.com>
2020-07-23 12:19:32 +0530

    changes

--- blog/settings.py
+++ blog/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'board',
+        'USER': 'board_user',
+        'PASSWORD': 'g00dluck',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
